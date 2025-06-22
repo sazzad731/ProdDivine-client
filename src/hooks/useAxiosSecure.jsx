8620f1f8f2e96348005495de3af2e5b0913478fe
sazzad731 <sazzadho731@gmail.com>
2025-06-23 04:41:22 +0600

    Change Base url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import Swal from 'sweetalert2';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://prod-divine-server.vercel.app/",
 });
 
 const useAxiosSecure = () => {
