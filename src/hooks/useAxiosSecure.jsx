0c585f59d5afbaf4a91c6f854579123c10626016
sazzad731 <sazzadho731@gmail.com>
2025-10-25 19:21:45 +0600

    [MODIFED]: added conditional rendering in Bookmarked.jsx

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,8 +4,8 @@ import useAuth from './useAuth';
 import Swal from 'sweetalert2';
 
 const axiosInstance = axios.create({
-  // baseURL: "https://prod-divine-server.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://prod-divine-server.vercel.app",
+  // baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
